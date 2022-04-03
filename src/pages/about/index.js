1b029f1a265a1f442a4cd80d92a237f0fb128d79
Alina Markova <1notablets1@gmail.com>
2022-04-03 15:32:18 +0300

    Create about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,15 @@
+import styles from "./index.module.css";
+
+import Block1 from "./../../components/block-1";
+import Block2 from "./../../components/block-2";
+
+function About() {
+  return (
+    <>
+      <Block1 />
+      <Block2 />
+    </>
+  );
+}
+
+export default About;
