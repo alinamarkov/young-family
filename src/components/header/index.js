341691cdd578d5475737f59cecbd9cebf757d73c
Alina Markova <1notablets1@gmail.com>
2022-04-15 12:19:43 +0300

    Create header components

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,12 +6,12 @@ function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img src="/logo.svg" alt="Young Family" />
+        <img src="/logo.svg" alt="Young Family" height="100%" />
       </div>
 
       <div className={styles.buttons}>
-        <NavButton text="О нас" active={true} />
-        <NavButton text="Няни" active={false}/>
+        <NavButton path="/" text="О нас" active={true} />
+        <NavButton path="/nanny" text="Няни" active={false}/>
       </div>
       
       <h1 className={styles.number}>+7 (999) 999-99-99</h1>
